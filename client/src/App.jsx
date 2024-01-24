fda61e834be126e748d6be2c1a5f95e4127a1495
TALLURI MANOJ KUMAR <106367643+manojtalluri12@users.noreply.github.com>
2024-01-24 18:05:45 +0530

    Make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,33 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Dashboard from "./pages/Dashboard"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Projects from "./pages/Projects"
-import Header from "./components/Header"
-import FooterCom from "./components/Footer"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import FooterCom from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/signin" element={<SignIn/>}/>
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/projects" element={<Projects/>}/>
-    </Routes>
-    <FooterCom/>
-    </BrowserRouter>
-  )
-}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
 
-export default App
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <FooterCom />
+    </BrowserRouter>
+  );
+};
+
+export default App;
