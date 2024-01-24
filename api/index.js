c5b1c5f6b59781dc2febdd035c75a8abcc9bd3e5
TALLURI MANOJ KUMAR <106367643+manojtalluri12@users.noreply.github.com>
2024-01-24 09:48:38 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('db is connected');
@@ -13,3 +15,5 @@ const app=express();
 app.listen(3000,()=>{
     console.log('server runing........');
 })
+
+app.use('/api/user',userRoutes)
